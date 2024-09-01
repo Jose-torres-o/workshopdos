fe65e61e5accdcd3137fc5d2a679f83a393a3419
jose-torres-o <torresoliverajose@gmail.com>
2024-08-31 20:37:46 -0500

    Implementado el método isValidHtml en HtmlValidator para validar HTML

--- workshopEstructuraDatos/src/HtmlValidator.java
+++ workshopEstructuraDatos/src/HtmlValidator.java
@@ -7,10 +7,35 @@ public class HtmlValidator {
 	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
 
 		/* IMPLEMENT THIS METHOD! */
+		//para crear un pila y rastrear las etiquetas de apertura.
 
-		return null; // this line is here only so this code will compile if you don't modify it
+		Stack<HtmlTag> stack = new Stack<>();
+
+		//extrae etiquetas una por una de la cola para verificar cada formato de etiqueta.
+		while (!tags.isEmpty()){
+			HtmlTag tag = tags.poll();
+
+			// si no es etiqueta de cerrada y no es de autocierre se agregará a la pila
+			if (tag.isOpenTag()&& !tag.isSelfClosing()) {
+				stack.push(tag);
+
+				// omitir etiquetas de autocerrado, ya que no afectan en nada en la pila
+			} else if (tag.isSelfClosing()) {
+				continue;
+
+				// si es etiqueta de cierre, que verifique si está vacía o si no coincide la etiqueta superior con la de cierre.
+			} else if (stack.isEmpty()|| !stack.peek().matches(tag)) {
+				//Si la pila está vacía o no coincide, devuelve la pila con las etiquetas no cerradas.
+
+				return stack;
+			} else {
+				//Si coincide, elimina la etiqueta de apertura de la pila.
+				stack.pop();
+			}
+		}
+		//Retorna una pila vacía para indicar que el HTML es válido.
+		return stack.isEmpty() ? new Stack<>() : stack;
+
 	}
-	
-
 }
 
